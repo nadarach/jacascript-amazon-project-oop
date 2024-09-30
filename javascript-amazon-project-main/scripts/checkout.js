531767c0178cb69a28d07aefa1309b4a147e504f
nadarach <hn_rachedi@esi.dz>
2024-09-30 19:34:13 +0100

    use Promise.all in checkout.js

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -4,8 +4,26 @@ import {cart} from '../data/cart-class.js';
 import { loadCart } from "../data/cart-class.js";
 import { loadProducts } from "../data/products-class.js";
 
+///// Handling asynchronous code using promises and Promise.all
+Promise.all([
+  new Promise(resolve => {
+    loadProducts(() => {
+      resolve();
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    })
+  })
+]).then(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+
 
 ///// Handling asynchronous code using promises
+/*
 new Promise((resolve) => {
   loadProducts(() => {
     resolve();
@@ -19,7 +37,8 @@ new Promise((resolve) => {
 }).then(() => {
   renderOrderSummary();
   renderPaymentSummary();
-})
+});
+*/
 
 
 ///// Handling asynchronous code using callback functions
