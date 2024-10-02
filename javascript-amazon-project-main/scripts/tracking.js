66bfd6bedc680c55f3e39483b9f52540e82ee7df
nadarach <hn_rachedi@esi.dz>
2024-10-02 17:53:16 +0100

    make the tracking page interactive (add progress tracking functionality)

--- javascript-amazon-project-main/scripts/tracking.js
+++ javascript-amazon-project-main/scripts/tracking.js
@@ -1,6 +1,9 @@
 import { getOrder, getOrderProduct } from "../data/orders.js";
 import { getProduct, loadProductsFetch } from "../data/products-class.js";
 import { formatDate } from "./utils/time.js";
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js' //default export
+
+
 
 await loadProductsFetch();
 
@@ -13,10 +16,13 @@ const product = getProduct(productId);
 
 const productDetails = getOrderProduct(orderId, productId);
 
-const orderTimeString = formatDate(order.orderTime);
-const deliveryTimeString = formatDate(productDetails.estimatedDeliveryTime);
+const orderTime = dayjs(order.orderTime);
+const deliveryTime = dayjs(productDetails.estimatedDeliveryTime);
 
-console.log(product, order, productDetails);
+const currentTime = dayjs();
+
+
+const progressPercent = Math.round(((currentTime.diff(dayjs(orderTime)) ) / (dayjs(deliveryTime).diff(orderTime))) * 100);
 
 let trackingHTML = '';
 
@@ -26,7 +32,7 @@ trackingHTML = `
   </a>
 
   <div class="delivery-date">
-    Arriving on ${deliveryTimeString}
+    Arriving on ${formatDate(deliveryTime)}
   </div>
 
   <div class="product-info">
@@ -40,19 +46,23 @@ trackingHTML = `
   <img class="product-image" src=${product.image}>
 
   <div class="progress-labels-container">
-    <div class="progress-label">
+    <div class="progress-label 
+      ${(progressPercent > 0 && progressPercent <= 49)? 'current-status': ''}">
       Preparing
     </div>
-    <div class="progress-label current-status">
+    <div class="progress-label
+      ${(progressPercent > 50 && progressPercent <= 99)? 'current-status': ''}">
       Shipped
     </div>
-    <div class="progress-label">
+    <div class="progress-label
+      ${(progressPercent >= 100)? 'current-status': ''}">
       Delivered
     </div>
   </div>
 
   <div class="progress-bar-container">
-    <div class="progress-bar"></div>
+    <div class="progress-bar" style="width:${progressPercent}%"></div>
   </div>`;
 
   document.querySelector('.js-order-tracking').innerHTML = trackingHTML;
+
